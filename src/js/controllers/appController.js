6c806b5e9ea5930f7ad989ed08800ddfdf852817
Andrei Teodorescu <andrei@globalmouth.com>
2015-12-14 17:52:51 +0100

    KSBWI-46 Updated twitter configurations

--- src/js/controllers/appController.js
+++ src/js/controllers/appController.js
@@ -11,53 +11,31 @@
 
       $scope.defaultHeight = 450;
 
-      $scope.twitter_timeline = {};
-
-      /**
-       * timeline configuration.
-       * parameter is the url parameter used in widget href. In case of user timeline, the value should be the users id.
-       * For search timeline, possible searches :
-       *    - #TwitterStories (hashtagsearch)
-       *    - manchester (string search)
-       *    - @twitterApi (mentions search)
-       * More at https://dev.twitter.com/web/embedded-timelines/search
-       * @type Object
-       */
-      var timeline = {
-         search: {
-            parameter: 'search?q=', //
-            value: '%23manchester%20united',
-            widget_id: '676378780357763072'
-         },
-         user: {
-            parameter: '',
-            value: false,
-            widget_id: 676406893443686401
-         }
-      };
-
       /**
        * setTimeline
        * Parse timeline configuration and init the twitter framework
        */
       var setTimeline = function () {
-         // Return the first enabled timeline
-         for ( var twitterItem in timeline ) {
-            if ( timeline[twitterItem].value !== false ) {
-               $scope.twitter_timeline = timeline[twitterItem];
-               $scope.twitter_timeline.height = $scope.currentHeight - 50;
-               break;
-            }
-         }
 
          $timeout(function () {
             $scope.$apply();
 
             if ( twttr ) {
-               // Force twitter bootstrap the widget
-               twttr.widgets.load(
-                  document.getElementById("timeline_twitter")
-               );
+               var twitterParams = {
+                  width: '100%',
+                  height: $scope.currentHeight - 50,
+                  chrome: 'noheader noborders transparent'
+               };
+               twitterParams = angular.merge(twitterParams, $scope.args.twitter);
+               twitterParams[$scope.args.twitter.type] = $scope.args.twitter.value ? encodeURIComponent($scope.args.twitter.value) : '';
+
+               twttr.widgets.createTimeline(
+                  twitterParams.widget_id,
+                  document.getElementById('timeline_twitter'),
+                  twitterParams
+               ).then(function (el) {
+
+               });
             }
          }, 100);
       };
@@ -66,6 +44,18 @@
       // The init-method returns a promise that resolves when all of the configurations are set, for instance the $scope.args variables
       // so we can call our methods that require parameters from the widget settings after the init method is called
       $scope.init().then(function () {
+
+         /**
+          * Default twitter object configuration.
+          * Possible values for @type : related | screenName | userId | favoritesScreenName | favoritesUserId
+          * @type {{value: string, widget_id: string, type: string}}
+          */
+         $scope.args.twitter = {
+            value: '#manchester united',
+            widget_id: '676378780357763072',
+            type: 'related'
+         };
+
          setTimeline();
       });
 
